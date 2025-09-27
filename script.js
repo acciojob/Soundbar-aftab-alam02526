4771e3a0ca725e8652bb53e2920cff9b5dcf70e0
aftab-alam02526 <aftab0447@gmail.com>
2025-09-27 17:24:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+// List of 7 sounds (names should match the files in 'sounds' folder)
+const sounds = ['sound1', 'sound2', 'sound3', 'sound4', 'sound5', 'sound6', 'sound7'];
+
+const container = document.getElementById('soundButtons');
+
+sounds.forEach(sound => {
+  // Create button for each sound
+  const btn = document.createElement('button');
+  btn.innerText = sound.charAt(0).toUpperCase() + sound.slice(1); // Capitalize
+
+  // Play sound on click
+  btn.addEventListener('click', () => {
+    const audio = new Audio(`sounds/${sound}.mp3`);
+    audio.play();
+  });
+
+  container.appendChild(btn);
+});
